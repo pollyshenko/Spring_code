fd1d4583fd0a36a5ccdde47b3710680009efbf1b
pollyshenko <pollyshenko@gmail.com>
2023-10-24 21:42:21 +0400

    add files

---
+++ src/test/java/com/example/testProj/TestProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
